1ee590fc660e23868efe3474d62601d628cd9e1a
Hakob650 <hakob11meneshian@gmail.com>
2025-03-08 14:15:07 +0400

    Update myprogram2.c

--- myprojects.c/myprogram2.c
+++ myprojects.c/myprogram2.c
@@ -7,6 +7,6 @@ years=days/365;
 days=days%365;
 weeks=days/7;
 remaining_days=days%7;
-printf("%d days is approximately %d years %d weeks and %d days.\n",days,years,weeks,remaining_days);
+printf("%d days is  %d years %d weeks and %d days.\n",days,years,weeks,remaining_days);
 return 0;
 }
