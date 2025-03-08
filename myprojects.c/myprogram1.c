1ad8cd0706f07ce98e50da9ff8a557ec38f0a4db
Hakob650 <hakob11meneshian@gmail.com>
2025-03-08 14:13:37 +0400

    Update myprogram1.c

--- myprojects.c/myprogram1.c
+++ myprojects.c/myprogram1.c
@@ -2,13 +2,13 @@
 int main(){
 int a=1,b=3,temp;
 printf("Before swap:\n");
-printf("a=%d",a);
-printf("b=%d",b);
+printf("%d",a);
+printf("%d",b);
 temp=a;
 a=b;
 b=temp;
 printf("After swap:\n");
-printf("a = %d\n", a);
-printf("b = %d\n", b);
+printf("%d\n", a);
+printf("%d\n", b);
 return 0;
 }
