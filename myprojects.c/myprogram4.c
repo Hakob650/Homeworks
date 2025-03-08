7eeed89886f300d52b1939aebc58b32d78b35ca5
Hakob650 <hakob11meneshian@gmail.com>
2025-03-08 14:14:36 +0400

    Update myprogram4.c

--- myprojects.c/myprogram4.c
+++ myprojects.c/myprogram4.c
@@ -7,6 +7,6 @@ int main() {
  total_days = total_days % 365;   
  months = total_days / 30;     
  days = total_days % 30;          
- printf("%d days is approximately %d years, %d months, and %d days.\n", total_days, years, months, days);
+ printf("%d days is %d years, %d months, and %d days.\n", total_days, years, months, days);
  return 0;
 }
