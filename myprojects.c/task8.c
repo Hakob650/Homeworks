cb41eb6d817bfc67136581d426d3528c49dd9817
Hakob650 <hakob11meneshian@gmail.com>
2025-03-25 17:37:29 +0400

    Update task8.c

--- myprojects.c/task8.c
+++ myprojects.c/task8.c
@@ -5,15 +5,10 @@ int main() {
     printf("Enter a number: ");
     scanf("%d", &num);
     int original_num = num;
-    if (num < 0) {
-        num = -num;
-    } for (; num != 0; num /= 10) {
+    for (; num != 0; num /= 10) {
         int digit = num % 10; 
         reversed = reversed * 10 + digit;
-    } if (original_num < 0) {
-        reversed = -reversed;
-    }
-    printf("Reversed number: %d\n", reversed);
+    } printf("Reversed number: %d\n", reversed);
      return 0;
 }
 
