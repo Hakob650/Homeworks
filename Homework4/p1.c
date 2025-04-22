760121afe2f2d1644ab9295f41a63a04f450a7a4
Hakob650 <hakob11meneshian@gmail.com>
2025-04-22 23:24:03 +0400

    Add files via upload

--- Homework4/p1.c
+++ Homework4/p1.c
@@ -1,1 +1,11 @@
-
+#include <stdio.h>
+int main(){
+    int count=0,number=0;
+    printf("enter number");
+    scanf("%d",&number);
+    while(number){
+	count+=number&1;
+        number>>=1;
+    }printf("1eri qanak: %d\n",count);
+     return 0;
+}    
