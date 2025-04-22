2d012e425af013b201b87b62f168de5b3b27e491
Hakob650 <hakob11meneshian@gmail.com>
2025-04-22 23:40:36 +0400

    Add files via upload

--- Homework6/work1.c
+++ Homework6/work1.c
@@ -1,1 +1,15 @@
-
+#include <stdio.h>
+int main(){
+    int num,sum=0;
+    printf("enter a number");
+    scanf("%d",&num);
+    for(int i=1;i<num/2;i++){
+         if(num%i==0){
+           sum+=i;
+         }
+    }if(sum==num){
+        printf("perfect");
+    }else{
+        printf("not perfect");
+    }return 0;
+}    
