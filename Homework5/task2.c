2e8225902cfdd2eb89d6e1260c28406807ad7538
Hakob650 <hakob11meneshian@gmail.com>
2025-04-22 23:37:48 +0400

    Add files via upload

---
+++ Homework5/task2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+printf("loop for 0-10: \n");
+int i=0;
+do{
+   printf("%d",i);
+   i++;
+}while(i<=10);
+
+printf("loop 10-0: \n");
+for(i=10;i>=0;i--){
+printf("%d",i);
+}return 0;
+}
+	
